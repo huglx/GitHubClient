a0b53d642af766be20062ec1dda2219c151168b0
Steel <isteeldeveloper@gmail.com>
2019-10-06 12:45:22 +0300

    commit activity bug was fixed

--- app/src/main/java/ru/gdgkazan/githubmvp/screen/commits/CommitsPresenter.java
+++ app/src/main/java/ru/gdgkazan/githubmvp/screen/commits/CommitsPresenter.java
@@ -1,12 +1,17 @@
 package ru.gdgkazan.githubmvp.screen.commits;
 
 import android.support.annotation.NonNull;
+import android.util.Log;
 
 import ru.arturvasilov.rxloader.LifecycleHandler;
 import ru.gdgkazan.githubmvp.R;
 import ru.gdgkazan.githubmvp.content.Repository;
 import ru.gdgkazan.githubmvp.repository.RepositoryProvider;
 import ru.gdgkazan.githubmvp.screen.repositories.RepositoriesView;
+import ru.gdgkazan.githubmvp.utils.PreferenceUtils;
+import rx.Observable;
+import rx.Subscription;
+import rx.functions.Func1;
 
 /**
  * @author Artur Vasilov
@@ -22,13 +27,25 @@ public class CommitsPresenter {
         mView = view;
     }
 
+    Observable.Transformer<Observable<String>, String> transformIntegerToString() {
+        return observable -> observable.map(String::valueOf);
+    }
+
     public void init(String repoName) {
+        final String[] user = new String[1];
+        PreferenceUtils.getUserName()
+                .subscribe(value -> user[0] = value);
+        //  Log.i("USER", user);
         RepositoryProvider.provideGithubRepository()
-                .commits("steel4132", repoName)
+                .commits(user[0], repoName)
                 .doOnSubscribe(mView::showLoading)
                 .doOnTerminate(mView::hideLoading)
                 .compose(mLifecycleHandler.load(R.id.repositories_request))
                 .subscribe(mView::showCommits, throwable -> mView.showError());
+    }
+
+    private String returnUser(String value) {
+        return value;
     }
 
     public void refresh(String repoName) {
